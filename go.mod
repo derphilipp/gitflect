6a71dde291f3fabddacfb16e538d7b610c520e26
Philipp Weißmann <philipp.weissmann@siemens.com>
2019-11-11 13:55:21 +0100

    WIP

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/derphilipp/gitflection
+
+go 1.13
+
+require (
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/vbauerster/mpb/v4 v4.11.0
+	gopkg.in/src-d/go-git.v4 v4.13.1
+	gopkg.in/yaml.v3 v3.0.0-20191107175235-0b070bb63a18
+)
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"sync"
+	"time"
 
 	"log"
 	"os"
@@ -13,6 +15,9 @@ import (
 	"gopkg.in/src-d/go-git.v4/config"
 	"gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
 	"gopkg.in/yaml.v3"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 )
 
 const githubURL = "https://github.com/"
@@ -20,6 +25,18 @@ const githubURL = "https://github.com/"
 var (
 	configFile = flag.String("config-file", "config.yaml", "path to custom configuration file")
 	// mazeFile   = flag.String("maze-file", "maze01.txt", "path to a custom maze file")
+)
+
+type gitStatus string
+
+const (
+	statusDone        = "✅ project updated"
+	statusDoneNothing = "✅ already up to date"
+	statusDownload    = "⏬ downloading"
+	statusUpload      = "⏫ uploading"
+	statusError       = "🚨 error"
+	statusUnknown     = "❓unknown"
+	statusWaiting     = "⌛ waiting"
 )
 
 type Project struct {
@@ -55,12 +72,12 @@ func loadConfig() error {
 
 	sshFile, err := ioutil.ReadFile(cfg.SSHKey)
 	if err != nil {
-		fmt.Printf("Could not load %s:\n", cfg.SSHKey)
+		log.Printf("Could not load %s:\n", cfg.SSHKey)
 		return err
 	}
 	publicKey, err := ssh.NewPublicKeys("git", []byte(sshFile), "")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return err
 	}
 	PublicKey = publicKey
@@ -68,45 +85,63 @@ func loadConfig() error {
 	return nil
 }
 
-func clone(url, localPath string) error {
+func clone(url, localPath string, reponame string) error {
+	updateState(statusDownload, reponame)
 	//	null, _ := os.Open(os.DevNull)
 	err := os.MkdirAll(localPath, os.ModePerm)
 	if err == os.ErrExist {
 	} else if err != nil {
-		fmt.Println("Could not create directory")
+		addOutputLine("Could not create directory", reponame)
+		updateState(statusError, reponame)
 		return err
 	}
 	// Clone
 	_, err = git.PlainClone(localPath, true, &git.CloneOptions{
 		URL:      url,
-		Progress: os.Stdout,
+		Progress: nil,
 	})
 
 	if err == git.ErrRepositoryAlreadyExists {
-		log.Printf("Repo already exists, nice...")
+		addOutputLine("Repo already exists, nice...", reponame)
+		//		log.Printf("Repo already exists, nice...")
 	} else if err != nil {
-		log.Printf("Cloning failed: %v\n", err)
+		addOutputLine(fmt.Sprintf("Cloning Failed: %v", err), reponame)
+		updateState(statusError, reponame)
+		//		log.Printf("Cloning failed: %v\n", err)
 		return err
 	}
 	return nil
 }
 
-func loadRepo(path string) (*git.Repository, error) {
+func loadRepo(path string, reponame string) (*git.Repository, error) {
 	repo, err := git.PlainOpen(path)
 	if err != nil {
-		log.Printf("Loading repository failed: %v\n", err)
+		addOutputLine(fmt.Sprintf("Loading repository failed: %v\n", err), reponame)
+		updateState(statusError, reponame)
 		return nil, err
 	}
 	return repo, nil
 }
 
-func fetch(repo *git.Repository) error {
+func fetch(repo *git.Repository, reponame string) error {
 	// Fetch
-	err := repo.Fetch(&git.FetchOptions{})
+	updateState(statusDownload, reponame)
+	err := repo.Fetch(&git.FetchOptions{
+		RemoteName: "origin",
+		Force:      true,
+		Tags:       git.AllTags,
+		RefSpecs: []config.RefSpec{
+			config.RefSpec("+refs/heads/*:refs/heads/*"),
+			config.RefSpec("+refs/pull/*/head:refs/pull/*/head"),
+		},
+	})
 	if err == git.NoErrAlreadyUpToDate {
-		log.Printf("Already up to date")
+		addOutputLine(fmt.Sprintf("Already up to date"), reponame)
+		//		log.Printf("Already up to date")
+		return nil
 	} else if err != nil {
-		log.Printf("Fetching existing repo failed: %v\n", err)
+		addOutputLine(fmt.Sprintf("Fetching existing repo failed: %v\n", err), reponame)
+		updateState(statusError, reponame)
 		return err
 	}
 	return nil
@@ -119,33 +154,129 @@ func setRemote(repo *git.Repository, targetURL string) error {
 	return err
 }
 
-func push(repo *git.Repository) error {
+func push(repo *git.Repository, reponame string) error {
 	// Push
 	err := repo.Push(&git.PushOptions{
 		RemoteName: "gittig",
+		Prune:      true,
 		Auth:       PublicKey,
 	})
+
+	if err == git.NoErrAlreadyUpToDate {
+		updateState(statusDoneNothing, reponame)
+		return nil
+	} else if err == nil {
+		updateState(statusDone, reponame)
+	}
+	updateState(statusError, reponame)
+	addOutputLine(fmt.Sprintf("Error occured: [%v](fg:red)", err), reponame)
 	return err
 }
+func remove(s []string, r string) []string {
+	for i, v := range s {
+		if v == r {
+			return append(s[:i], s[i+1:]...)
+		}
+	}
+	return s
+}
+
+var activeRepositories []string
+var completedRepositories []string
 
 func processProject(url, localname string) error {
-	fmt.Printf("Processing Project: %s\n\n", localname)
+	activeRepositories = append(activeRepositories, localname)
+	addOutputLine("Processing Project", localname)
 
 	targetURL := cfg.DefaultURL + localname
 	projectPath := cfg.DataPath + localname
+	updateState(statusWaiting, localname)
+	clone(url, projectPath, localname)
+	repo, err := loadRepo(projectPath, localname)
+	if err != nil {
+		updateState(statusError, localname)
+		return errors.New("Loading Repo failed")
+	}
+	fetch(repo, localname)
+	if err != nil {
+		updateState(statusError, localname)
+		return errors.New("Fetching Repo failed")
+	}
 
-	clone(url, projectPath)
-	repo, err := loadRepo(projectPath)
-	fetch(repo)
 	repo.DeleteRemote("gittig")
 	setRemote(repo, targetURL)
-	push(repo)
 
-	if err != nil {
-		log.Printf("Pushing failed!: %v\n", err)
-		return err
+	updateState(statusUpload, localname)
+	err = push(repo, localname)
+	return err
+}
+
+func startProcessing() {
+	wg := sync.WaitGroup{}
+	for projectkey := range cfg.Projects {
+		project := cfg.Projects[projectkey]
+		if cfg.Parallel {
+			go processProject(project.OriginURL, project.LocalName)
+			addActiveRepo(project.LocalName)
+			wg.Add(1)
+		} else {
+			processProject(project.OriginURL, project.LocalName)
+		}
 	}
-	return nil
+
+	for _, project := range cfg.GithubProjects {
+		if cfg.Parallel {
+			go processProject(githubURL+project, project)
+			addActiveRepo(project)
+			wg.Add(1)
+		} else {
+			processProject(githubURL+project, project)
+		}
+	}
+
+}
+
+var grid *ui.Grid
+var repoList *widgets.Table
+var textList *widgets.List
+
+func addOutputLine(text, projectname string) {
+	newEntry := fmt.Sprintf("[%s](fg:blue):\t %s", projectname, text)
+	textList.Rows = append(textList.Rows, newEntry)
+}
+
+func addActiveRepo(projectname string) {
+	newEntry := []string{projectname, "active"}
+	repoList.Rows = append(repoList.Rows, newEntry)
+}
+
+func findRowIndex(text string, table *widgets.Table) int {
+	for i, row := range table.Rows {
+		if row[0] == text {
+			return i
+		}
+	}
+	return -1
+}
+
+func updateState(newstate, projectname string) {
+	i := findRowIndex(projectname, repoList)
+	if i != -1 {
+		repoList.Rows[i][1] = newstate
+	} else {
+		repoList.Rows = append(repoList.Rows, []string{projectname, newstate})
+	}
+}
+
+func updateGridEnv() {
+	termWidth, termHeight := ui.TerminalDimensions()
+	grid.SetRect(0, 0, termWidth, termHeight)
+	grid.Set(
+		ui.NewRow(1,
+			ui.NewCol(1.0/3, repoList),
+			ui.NewCol(2.0/2, textList),
+		),
+	)
 }
 
 func main() {
@@ -159,27 +290,79 @@ func main() {
 		log.Printf("Data directory does not exist.")
 	}
 
-	wg := sync.WaitGroup{}
-	for projectkey := range cfg.Projects {
-		project := cfg.Projects[projectkey]
-		if cfg.Parallel {
-			go processProject(project.OriginURL, project.LocalName)
-			wg.Add(1)
-		} else {
-			processProject(project.OriginURL, project.LocalName)
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+	uiEvents := ui.PollEvents()
+
+	repoList = widgets.NewTable()
+	repoList.Title = "Active Elements"
+	repoList.Rows = [][]string{{"Repo", "Status"}}
+	repoList.RowSeparator = false
+	//	repoList.TextStyle = ui.NewStyle(ui.ColorYellow)
+	repoList.SetRect(0, 0, 60, 75)
+
+	textList = widgets.NewList()
+
+	textList.Title = "Log output"
+	textList.WrapText = true
+	//textList.RowSeparator = false
+	//textList.Rows = [][]string{{"Repo", "Text"}}
+	//	repoList.TextStyle = ui.NewStyle(ui.ColorYellow)
+
+	textList.SetRect(61, 0, 160, 75)
+
+	grid = ui.NewGrid()
+
+	drawFunction()
+	//	ui.Render(repoList, textList)
+
+	go startProcessing()
+
+	ticker := time.NewTicker(time.Second).C
+
+	for {
+		select {
+		case e := <-uiEvents:
+			switch e.ID { // event string/identifier
+			case "q", "<C-c>": // press 'q' or 'C-c' to quit
+				return
+				/*case "<MouseLeft>":
+					payload := e.Payload.(ui.Mouse)
+					x, y := payload.X, payload.Y
+				case "<Resize>":
+					payload := e.Payload.(ui.Resize)
+					width, height := payload.Width, payload.Height
+				}
+				switch e.Type {
+				case ui.KeyboardEvent: // handle all key presses
+					eventID = e.ID // keypress string
+				}*/
+				// use Go's built-in tickers for updating and drawing data
+
+			}
+		case <-ticker:
+			drawFunction()
+
 		}
 	}
+}
 
-	for _, project := range cfg.GithubProjects {
-		if cfg.Parallel {
-			go processProject(githubURL+project, project)
-			wg.Add(1)
-		} else {
-			processProject(githubURL+project, project)
-		}
+func drawFunction() {
+	updateGridEnv()
+	if len(textList.Rows) > 0 {
+		textList.ScrollBottom()
 	}
-	if cfg.Parallel {
-		wg.Wait()
+	ui.Render(grid)
+}
+
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
 	}
 
+	_, ok := set[item]
+	return ok
 }
